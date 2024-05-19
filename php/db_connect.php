6e7cda0417730f5f3d98e7a90c10445ebc999b1a
speedjin1995 <speedjin1995@gmail.com>
2024-05-19 23:19:01 +0800

    change to point to server

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Kuala_Lumpur');
-$db = mysqli_connect("localhost", "root", "", "sosexpress");
+$db = mysqli_connect("srv909.hstgr.io", "u782565293_sosexpress", "Aa@111222333", "u782565293_sosexpress");
 
 if(mysqli_connect_errno()){
     echo 'Database connection failed with following errors: ' . mysqli_connect_error();
